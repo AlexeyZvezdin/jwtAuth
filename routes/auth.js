eb11ee1e8ac48cb879dba6e17772b2904729712f
Alexey Zvezdin <alexeyrush94@gmail.com>
2019-06-01 02:17:16 +0400

    making a login

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,29 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
+  //  VALIDATE THE DATA BEFORE WE A USER
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Check if the user is already in the databasae
+
+  // prettier-ignore
+  const emailExist = await User.findOne({email: req.body.email});
+  if (emailExist) return res.status(400).send("Email already exist");
+
+  // Hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  //  Create a user
   const user = new User(
     {
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password
+      password: hashedPassword
     },
     () => {
       console.log("Pended");
@@ -14,12 +31,12 @@ router.post("/register", async (req, res) => {
   );
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.send({ user: user.id });
   } catch (err) {
     res.status(400).send(err);
   }
 });
 
-// router.post('/login', )
+router.post("/login");
 
 module.exports = router;
