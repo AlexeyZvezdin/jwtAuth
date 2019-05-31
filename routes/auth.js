7667fc0f3e680716a1120ccce682deddfa199dcb
Alexey Zvezdin <alexeyrush94@gmail.com>
2019-06-01 03:01:01 +0400

    adding jwt

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
-const { registerValidation } = require("../validation");
+const { registerValidation, loginValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
   //  VALIDATE THE DATA BEFORE WE A USER
@@ -37,6 +37,21 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/login");
+// LOGIN
+router.post("/login", async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Check if user exist
+  // prettier-ignore
+  const user = await User.findOne({email: req.body.email});
+  if (!user) return res.status(400).send("Email is invalid");
+
+  // Check if password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Invalid password");
+
+  res.send("Success and Loggen in!");
+});
 
 module.exports = router;
