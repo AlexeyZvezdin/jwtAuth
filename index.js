94e57adbfc299089204c0d2224350d21f81fb434
Alexey Zvezdin <alexeyrush94@gmail.com>
2019-06-01 04:46:53 +0400

    finished privaterouting

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 
 // Route middleware
 app.use("/api/user", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(3000, () => {
   console.log("Up and running");
